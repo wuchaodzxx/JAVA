c3853293c7d70bef6818a1ff48356bcb51ec9599
wuchaodzxx <wuchao19930417@qq.com>
2017-01-05 14:47:40 +0800

    f

--- LeetCode/src/T37/MainClass.java
+++ LeetCode/src/T37/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		String s = "hello";
-		 System.out.println((s.split(","))[0]);
+		 System.out.println((s.split(","))[0]); 
 	}
 
 }
